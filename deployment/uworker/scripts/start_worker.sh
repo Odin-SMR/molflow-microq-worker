7e14d1249bcf4ecb04b65a42f6900ab3b239486c
Joakim Möller <joakim.moller@molflow.com>
2020-11-13 15:54:43 +0100

    use dockerhub credentials

--- deployment/uworker/scripts/start_worker.sh
+++ deployment/uworker/scripts/start_worker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 export HOSTNAME=$(hostname)
 source uworker.conf
-docker login ${UWORKER_REGISTRY_URL} \
-       -u ${UWORKER_REGISTRY_USERNAME} -p ${UWORKER_REGISTRY_PASSWORD}
-docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD}
+docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_TOKEN}
 PYTHONPATH=/app python3 /app/uworker/uworker.py --no-command
--- deployment/uworker/setup_test_env.sh
+++ deployment/uworker/setup_test_env.sh
@@ -7,8 +7,7 @@ set -e
 
 login_to_registry() {
     source /root/uworker.conf
-    docker login ${UWORKER_REGISTRY_URL} \
-           -u ${UWORKER_REGISTRY_USERNAME} -p ${UWORKER_REGISTRY_PASSWORD}
+    docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_TOKEN}
 }
 
 start_uservice() {
